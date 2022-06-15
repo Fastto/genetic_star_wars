8c4a2bf781789543ec99bc89904a297519e3c421
Maksym Skorchenko <m.skorchenko@gmail.com>
2022-06-15 19:31:47 +0300

    add todo doc

--- Assets/Scripts/Data/State.cs
+++ Assets/Scripts/Data/State.cs
@@ -2,7 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class State : ScriptableObject
+public abstract class State : ScriptableObject
 {
+   protected ShipController _ship;
+   public bool IsFinished { get; protected set; }
    
+   public virtual void Init(ShipController ship)
+   {
+      _ship = ship;
+      Init();
+   }
+
+   protected virtual void Init()
+   {
+      
+   }
+
+   protected virtual bool ConditionToFinish()
+   {
+      return false;
+   }
+   
+   public virtual void Update()
+   {
+      if(IsFinished)
+         return;
+      
+      if(!ConditionToFinish())
+         Run();
+      else
+      {
+         IsFinished = true;
+         Finish();
+      }
+   }
+
+   protected virtual void Run()
+   {
+      
+   }
+   
+   protected virtual void Finish()
+   {
+      
+   }
 }
--- Assets/Scripts/ShipStateController.cs
+++ Assets/Scripts/ShipStateController.cs
@@ -1,18 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipStateController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private State idleState;
+    [SerializeField] private State battleState;
+    [SerializeField] private State miningState;
+    [SerializeField] private State unloadingState;
+    [SerializeField] private State followToBusyGoldState;
+    [SerializeField] private State followToFreeGoldState;
+    [SerializeField] private State followToEnemyState;
+    [SerializeField] private State followToStationState;
+    
+    private ShipController _ship;
+    private State _state;
+
+    private void Start()
     {
-        
+        _ship = GetComponent<ShipController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_state.IsFinished)
+        {
+            SetState(DetermineState());
+        }
+        else
+        {
+            _state.Update();
+        }
+    }
+
+    private void SetState(State state)
+    {
+        _state = Instantiate(state);
+        _state.Init(_ship);
+    }
+
+    private State DetermineState()
+    {
+        return idleState;
     }
 }

53d7f1df022941111de352450c27650d70a95129
Maksym Skorchenko <m.skorchenko@gmail.com>
2022-06-24 14:00:32 +0300

    Stub of station statistics

---
+++ Assets/Scripts/StationStatistics.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationStatistics : MonoBehaviour
+{
+    Quaternion rotation;
+    private Vector3 position;
+    void Awake()
+    {
+        rotation = transform.rotation;
+        position = transform.position;
+    }
+    void LateUpdate()
+    {
+        transform.rotation = rotation;
+        transform.position = position;
+    }
+    
+}
